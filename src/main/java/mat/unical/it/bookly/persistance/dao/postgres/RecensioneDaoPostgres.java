f1bdc6624a8f02171ea02f0ca82b78098800f3d5
Jacopo Garofalo <jacopogarofalo02@gmail.com>
2023-01-30 12:19:45 +0100

    Aggiustata query SQL per statistiche

--- src/main/java/mat/unical/it/bookly/persistance/dao/postgres/RecensioneDaoPostgres.java
+++ src/main/java/mat/unical/it/bookly/persistance/dao/postgres/RecensioneDaoPostgres.java
@@ -156,7 +156,7 @@ public class RecensioneDaoPostgres implements RecensioneDao {
     @Override
     public String findPreferredResultByAttribute(Long idUtente, String attribute) {
         List<String> listaAttributi = new ArrayList<>();
-        String query = "SELECT * FROM recensioni r WHERE EXISTS ( SELECT * FROM post p WHERE r.id = p.id AND EXISTS(SELECT * FROM utenti u WHERE p.utente = u.id AND u.id = ?))";
+        String query = "SELECT * FROM libro l WHERE EXISTS ( SELECT * FROM recensione r WHERE l.isbn = r.libro AND EXISTS( SELECT * FROM post p WHERE r.id = p.id AND EXISTS(SELECT * FROM utenti u WHERE p.utente = u.id AND u.id = ?)))";
         try{
             PreparedStatement st = conn.prepareStatement(query);
             st.setLong(1,idUtente);
