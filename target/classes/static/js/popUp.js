ae1f8423372e0aa90fd981625bd115e4c65de5c8
checcostra <strangisfrancesco2@gmail.com>
2023-02-01 10:53:00 +0100

    javascript ancora

--- target/classes/static/js/popUp.js
+++
@@ -1,5 +0,0 @@
-function showPopup() {
-    alert("Email correttamente inviata. Controlla nella tua casella di posta.");
-}
-
-document.querySelector('[type="submit"]').addEventListener("click", showPopup);
